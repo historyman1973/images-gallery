b024d4aaba1fc2b2305bf890da864d1238a6d067
Dave Mann <davemanndna@gmail.com>
2021-12-07 19:23:08 +0000

    Refresh project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,25 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import ImageCard from "./components/ImageCard";
-import { Container, Row, Col } from "react-bootstrap";
+import logo from './logo.svg';
+import './App.css';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-
-const App = () => {
-  const [word, setWord] = useState("");
-  const [images, setImages] = useState([]);
-
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    fetch(
-      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    setWord("");
-  };
-
-  const handleDeleteImage = (id) => {
-    setImages(images.filter((image) => image.id !== id));
-  };
-
+function App() {
   return (
-    <div>
-      <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <Container className="mt-4">
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            <Col key={i} className="pb-3">
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
