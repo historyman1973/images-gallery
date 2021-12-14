5b8855aa185b1a3cde6a41b30e770e3c06e89537
Dave Mann <davemanndna@gmail.com>
2021-12-14 16:39:40 +0000

    Enabled Python linter and formatter

--- api/main.py
+++ api/main.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
@@ -15,19 +15,18 @@ if not UNSPLASH_KEY:
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
 
     data = response.json()
     return data
 
+
 if __name__ == "__main__":
-    app.debug=True
+    app.debug = True
     app.run(host="0.0.0.0", port=5050)
