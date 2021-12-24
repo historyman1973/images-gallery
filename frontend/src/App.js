46bfc4c1110cfafcb73a0c604512796f46765d4c
Dave Mann <davemanndna@gmail.com>
2021-12-24 21:07:38 +0000

    Added Spinner on initial loading screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import ImageCard from "./components/ImageCard";
+import Spinner from "./components/Spinner";
 import { Container, Row, Col } from "react-bootstrap";
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
@@ -11,11 +12,13 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 const App = () => {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getSavedImages = async () => {
     try {
-      const res = await axios.get(`${API_URL}/images`)
+      const res = await axios.get(`${API_URL}/images`);
       setImages(res.data || []);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -72,7 +75,7 @@ const App = () => {
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {loading ? <Spinner /> : <><Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
       <Container className="mt-4">
         <Row xs={1} md={2} lg={3}>
           {images.map((image, i) => (
@@ -84,7 +87,8 @@ const App = () => {
             </Col>
           ))}
         </Row>
-      </Container>
+      </Container></>}
+
     </div>
   );
 };
