464528f3ae470f0b27c4ed8aa5987b9d831e42ee
Dave Mann <davemanndna@gmail.com>
2021-12-27 11:16:50 +0000

    Image author info

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Nav } from "react-bootstrap";
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
         <Card.Title>{image.title?.toUpperCase()}</Card.Title>
         <Card.Text>{image.description || image.alt_description}</Card.Text>
-        <footer className="blockquote-footer">{image.user.name}</footer>
-        <hr></hr>
         {!image.saved && (
-        <Button variant="danger" onClick={() => saveImage(image.id)}>
-          Save
-        </Button>
-        )}{' '}
+          <Button variant="danger" onClick={() => saveImage(image.id)}>
+            Save
+          </Button>
+        )}{" "}
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
         </Button>
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+          <Nav.Link href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
